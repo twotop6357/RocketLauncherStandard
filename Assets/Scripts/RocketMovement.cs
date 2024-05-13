052f8393fd2a54e23fecf76b296ace44c43e6e55
twotop6357 <twotop6357@gmail.com>
2024-05-13 10:33:21 +0900

    Add spaceInput
    스페이스바 인풋시스템은 넣었는데...

--- Assets/Scripts/RocketMovement.cs
+++ Assets/Scripts/RocketMovement.cs
@@ -19,7 +19,7 @@ public class RocketMovement : MonoBehaviour
     public void ApplyMovement(Vector2 direction)
     {
         // TODO : 회전을 적용하고 이동을 적용함 -> 이에 대한 구현을 아래에서 진행할 것
-        // Rotate(direction);
+        Rotate(direction);
         Move(direction);
         direction = direction * SPEED;
         _rb2d.velocity = direction;
@@ -33,7 +33,8 @@ public class RocketMovement : MonoBehaviour
     private void Rotate(Vector2 direction)
     {
         // TODO : 완만한 회전을 적용함
-        
+        direction = direction * ROTATIONSPEED;
+        _rb2d.transform.Rotate(direction);
     }
 
     private void Move(Vector2 direction)
